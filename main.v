96d33f06381c8ee9a7e935f1c2d2195551136cdb
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-01-03 16:57:36 +0530

    dd

--- main.v
+++ main.v
@@ -3,11 +3,24 @@ module elevator_top (
     input [2:0] req_floor,
     input emergency_stop,
     output [1:0] door,
-    output [2:0] current_floor,
+    output reg [2:0] current_floor,
     output [7:0] requests,
     output up,down,idle,
 );
+reg door_timer;
+reg emergency_stopped;
+reg flag=0;
 
+always @(req_floor) begin
 
-    
+requests[req_floor] = 1;
+
+if (max_request < req_floor) begin
+max_request = req_floor;
+end
+if (min_request > req_floor) begin
+min_request = req_floor;
+end
+
+end
 endmodule
