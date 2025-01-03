e8680bd69b937ad3e00450effd4c22a90edc8662
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-01-03 11:25:48 +0530

    dd

---
+++ main.v
@@ -0,0 +1,13 @@
+module elevator_top (
+    input clk,rst,
+    input [2:0] req_floor,
+    input emergency_stop,
+    output [1:0] door,
+    output [2:0] current_floor,
+    output [7:0] requests,
+    output up,down,idle,
+);
+
+
+    
+endmodule
